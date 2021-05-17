fa402e7afc72ba8a8a54cc75eb6025ad2d7ae87c
Preetham14321 <79894297+Preetham14321@users.noreply.github.com>
2021-05-17 17:18:59 +0530

    docs: update

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ const name1El = document.querySelector('#name--1');
 
 let scores, currentScore, activePlayer, playing;
 // taking user names
-// const name1 = prompt("Preetham Say's Insert PLAYER-1 Name?");
-// const name2 = prompt("Preetham say's Insert PLAYER-2 Name?");
-// name0El.textContent = name1;
-// name1El.textContent = name2;
+const name1 = prompt("Preetham Say's Insert PLAYER-1 Name?");
+const name2 = prompt("Preetham say's Insert PLAYER-2 Name?");
+name0El.textContent = name1;
+name1El.textContent = name2;
 
 // Starting conditions
 const init = function () {
